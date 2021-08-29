383cd03a9f6424bdc714c08bd47e0488074c1676
tr7zw <tr7zw@live.de>
2021-08-29 16:35:04 +0200

    Render the 3d layers in Firstperson. Fix #1

--- src/main/java/dev/tr7zw/skinlayers/mixin/PlayerRendererMixin.java
+++ src/main/java/dev/tr7zw/skinlayers/mixin/PlayerRendererMixin.java
@@ -5,15 +5,24 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import com.mojang.blaze3d.vertex.PoseStack;
+
 import dev.tr7zw.skinlayers.Settings;
+import dev.tr7zw.skinlayers.accessor.PlayerEntityModelAccessor;
+import dev.tr7zw.skinlayers.accessor.PlayerSettings;
+import dev.tr7zw.skinlayers.render.CustomizableModelPart;
 import dev.tr7zw.skinlayers.renderlayers.BodyLayerFeatureRenderer;
 import dev.tr7zw.skinlayers.renderlayers.HeadLayerFeatureRenderer;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.PlayerModel;
+import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.player.AbstractClientPlayer;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererProvider.Context;
 import net.minecraft.client.renderer.entity.LivingEntityRenderer;
 import net.minecraft.client.renderer.entity.player.PlayerRenderer;
+import net.minecraft.client.renderer.texture.OverlayTexture;
 
 @Mixin(PlayerRenderer.class)
 public abstract class PlayerRendererMixin extends LivingEntityRenderer<AbstractClientPlayer, PlayerModel<AbstractClientPlayer>> {
@@ -38,6 +47,33 @@ public abstract class PlayerRendererMixin extends LivingEntityRenderer<AbstractC
         playerModel.rightSleeve.visible = false;
         playerModel.leftPants.visible = false;
         playerModel.rightPants.visible = false;
+    }
+    
+    @Inject(method = "renderHand", at = @At("RETURN"))
+    private void renderHand(PoseStack poseStack, MultiBufferSource multiBufferSource, int i,
+            AbstractClientPlayer abstractClientPlayer, ModelPart arm, ModelPart sleeve, CallbackInfo info) {
+        PlayerSettings settings = (PlayerSettings) abstractClientPlayer;
+        float pixelScaling = 1.16f;
+        float armHeightScaling = 1.1f;
+        if(settings.getSkinLayers() != null) {
+            CustomizableModelPart part = null;
+            if(sleeve == this.model.leftSleeve) {
+                part = settings.getSkinLayers()[2];
+            }else {
+                part = settings.getSkinLayers()[3];
+            }
+            part.copyFrom(arm);
+            poseStack.pushPose();
+            poseStack.scale(pixelScaling, armHeightScaling, pixelScaling);
+            part.y -= 0.6;
+            part.render(poseStack,
+                multiBufferSource
+                        .getBuffer(RenderType.entityTranslucent(abstractClientPlayer.getSkinTextureLocation())),
+                i, OverlayTexture.NO_OVERLAY);
+            part.setPos(0, 0, 0);
+            part.setRotation(0, 0, 0);
+            poseStack.popPose();
+        }
     }
     
 }
