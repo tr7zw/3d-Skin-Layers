be592317ce90b7d9cb276b2c6a504405eae8aa28
tr7zw <tr7zw@live.de>
2022-09-22 21:47:14 +0200

    Also dump the textures

--- Shared/src/main/java/dev/tr7zw/skinlayers/exporter/MultiBufferObjConsumer.java
+++ Shared/src/main/java/dev/tr7zw/skinlayers/exporter/MultiBufferObjConsumer.java
@@ -2,7 +2,14 @@ package dev.tr7zw.skinlayers.exporter;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
+import org.lwjgl.opengl.GL15;
+
+import com.mojang.blaze3d.platform.NativeImage;
+import com.mojang.blaze3d.platform.NativeImage.Format;
+import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 
 import net.minecraft.client.renderer.MultiBufferSource;
@@ -14,6 +21,7 @@ public class MultiBufferObjConsumer implements MultiBufferSource {
     private ObjConsumer lastConsumer = null;
     private final File outputFolder;
     private int id = 0;
+    private Set<Integer> dumpedTextureIds = new HashSet<>();
     
     public MultiBufferObjConsumer(File outputFolder) {
         this.outputFolder = outputFolder;
@@ -33,6 +41,34 @@ public class MultiBufferObjConsumer implements MultiBufferSource {
         if(lastConsumer != null) {
             try {
                 lastConsumer.writeData(new File(outputFolder, id++ + ".obj"));
+                lastType.setupRenderState();
+                int textureId = 0;
+                int shaderId = 0;
+                do {
+                    textureId = RenderSystem.getShaderTexture(shaderId);
+                    shaderId++;
+                    if(textureId == 0 || dumpedTextureIds.contains(textureId)) {
+                        break;
+                    }
+                    dumpedTextureIds.add(textureId);
+                    GL15.glBindTexture(GL15.GL_TEXTURE_2D, textureId);
+                    float width = GL15.glGetTexLevelParameterf(GL15.GL_TEXTURE_2D, 0, GL15.GL_TEXTURE_WIDTH);
+                    float height = GL15.glGetTexLevelParameterf(GL15.GL_TEXTURE_2D, 0, GL15.GL_TEXTURE_HEIGHT);
+                    if(width <= 0 || height <= 0) {
+                        continue;
+                    }
+                    try(NativeImage img = new NativeImage(Format.RGBA, (int)width, (int)height, false)){
+                        img.downloadTexture(0, false);
+                        img.writeToFile(new File(outputFolder, "texture_" + textureId + ".png"));
+                    }
+                }while(textureId != 0);
+//                ByteBuffer buffer = ByteBuffer.allocate(50*1024*1024);
+//                GL15.glGetTexImage(GL15.GL_TEXTURE_2D, 0, GL15.GL_RGBA8, GL15.GL_BYTE, buffer);
+//                byte[] data = new byte[buffer.position()];
+//                buffer.rewind();
+//                buffer.get(data);
+//                Files.write(new File(outputFolder, id++ + ".png").toPath(), data);
+//                System.out.println("Saved image");
             } catch (IOException e) {
                 e.printStackTrace();
             }
--- Shared/src/main/java/dev/tr7zw/skinlayers/exporter/ObjConsumer.java
+++ Shared/src/main/java/dev/tr7zw/skinlayers/exporter/ObjConsumer.java
@@ -2,8 +2,6 @@ package dev.tr7zw.skinlayers.exporter;
 
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
