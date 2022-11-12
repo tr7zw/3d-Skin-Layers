4894b0f600cc17eb64676d87588a7269629b3f4b
tr7zw <tr7zw@live.de>
2022-11-12 20:16:41 +0100

    Make sure the value is not null

--- Shared/src/main/java/dev/tr7zw/skinlayers/SkinUtil.java
+++ Shared/src/main/java/dev/tr7zw/skinlayers/SkinUtil.java
@@ -67,8 +67,10 @@ public class SkinUtil {
                 HttpTextureAccessor httpTexture = (HttpTextureAccessor) texture;
                 try {
                     NativeImage img = httpTexture.getImage();
-                    cache.put(texture, img);
-                    return img;
+                    if(img != null) {
+                        cache.put(texture, img);
+                        return img;
+                    }
                 }catch(FileNotFoundException ex) {
                     //not there
                 }
@@ -76,10 +78,12 @@ public class SkinUtil {
             if(texture instanceof DynamicTexture) {
                 try {
                     NativeImage img = ((DynamicTexture) texture).getPixels();
-                    img.getPixelRGBA(0, 0); // check that it's allocated
-                    // Do not cache dynamic textures. It's a O(1) call to get them, and the cache would close them after 60 seconds
-                    //cache.put(texture, img);
-                    return img;
+                    if(img != null) {
+                        img.getPixelRGBA(0, 0); // check that it's allocated
+                        // Do not cache dynamic textures. It's a O(1) call to get them, and the cache would close them after 60 seconds
+                        //cache.put(texture, img);
+                        return img;
+                    }
                 }catch(Exception ex) {
                     // not backed by an image
                 }
