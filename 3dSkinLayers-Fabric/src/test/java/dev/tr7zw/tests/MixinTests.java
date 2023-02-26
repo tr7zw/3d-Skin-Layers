edbba1bd348a034664d4dd6f4a49996fad90cdbe
tr7zw <tr7zw@live.de>
2023-02-26 18:48:36 +0100

    Try to check the option menu texts

--- 3dSkinLayers-Fabric/src/test/java/dev/tr7zw/tests/MixinTests.java
+++ 3dSkinLayers-Fabric/src/test/java/dev/tr7zw/tests/MixinTests.java
@@ -1,17 +1,18 @@
 package dev.tr7zw.tests;
 
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Set;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.objenesis.Objenesis;
 import org.objenesis.ObjenesisStd;
 
-import com.mojang.realmsclient.util.SkinProcessor;
-
+import dev.tr7zw.config.CustomConfigScreen;
 import dev.tr7zw.skinlayers.Config;
 import dev.tr7zw.skinlayers.SkinLayersMod;
 import dev.tr7zw.skinlayers.SkinLayersModBase;
@@ -21,26 +22,17 @@ import dev.tr7zw.skinlayers.accessor.SkullModelAccessor;
 import dev.tr7zw.skinlayers.renderlayers.BodyLayerFeatureRenderer;
 import dev.tr7zw.skinlayers.renderlayers.HeadLayerFeatureRenderer;
 import net.minecraft.SharedConstants;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.OptionInstance;
 import net.minecraft.client.model.PlayerModel;
 import net.minecraft.client.model.SkullModel;
-import net.minecraft.client.model.geom.EntityModelSet;
-import net.minecraft.client.model.geom.ModelLayerLocation;
-import net.minecraft.client.model.geom.ModelPart;
-import net.minecraft.client.model.geom.PartPose;
-import net.minecraft.client.model.geom.builders.CubeDeformation;
-import net.minecraft.client.model.geom.builders.CubeListBuilder;
-import net.minecraft.client.model.geom.builders.PartDefinition;
 import net.minecraft.client.player.RemotePlayer;
 import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
 import net.minecraft.client.renderer.blockentity.SkullBlockRenderer;
-import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.LivingEntityRenderer;
-import net.minecraft.client.renderer.entity.EntityRendererProvider.Context;
 import net.minecraft.client.renderer.entity.layers.CustomHeadLayer;
 import net.minecraft.client.renderer.entity.layers.RenderLayer;
 import net.minecraft.client.renderer.entity.player.PlayerRenderer;
+import net.minecraft.locale.Language;
 import net.minecraft.server.Bootstrap;
 import net.minecraft.world.level.block.entity.SkullBlockEntity;
 
@@ -76,8 +68,7 @@ public class MixinTests {
     
     @Test
     public void checkInjectedPlayerLayers() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
-        EntityRendererProvider.Context context = new Context(null, null, null, null, null, new DummyModelSet(), null);
-        PlayerRenderer renderer = new PlayerRenderer(context, false);
+        PlayerRenderer renderer = TestUtil.getPlayerRenderer();
         Field field =  LivingEntityRenderer.class.getDeclaredField("layers");
         field.setAccessible(true);
         List<RenderLayer<?,?>> layers = (List<RenderLayer<?, ?>>) field.get(renderer);
@@ -95,24 +86,22 @@ public class MixinTests {
         assertTrue(foundHeadLayer);
     }
     
-    private class DummyModelSet extends EntityModelSet {
-
-        @Override
-        public ModelPart bakeLayer(ModelLayerLocation modelLayerLocation) {
-            PartDefinition part = PlayerModel.createMesh(CubeDeformation.NONE, false).getRoot();
-            part.getChild("head").addOrReplaceChild("jaw", CubeListBuilder.create(), PartPose.ZERO);
-            part.getChild("head").addOrReplaceChild("left_ear", CubeListBuilder.create(), PartPose.ZERO);
-            part.getChild("head").addOrReplaceChild("right_ear", CubeListBuilder.create(), PartPose.ZERO);
-            part.addOrReplaceChild("left_wing", CubeListBuilder.create(), PartPose.ZERO);
-            part.addOrReplaceChild("right_wing", CubeListBuilder.create(), PartPose.ZERO);
-            part.addOrReplaceChild("tail", CubeListBuilder.create(), PartPose.ZERO);
-            part.getChild("head").addOrReplaceChild("feather", CubeListBuilder.create(), PartPose.ZERO);
-            part.addOrReplaceChild("box", CubeListBuilder.create(), PartPose.ZERO);
-            return part.bake(0, 0);
+    @Test
+    public void langTests() throws Throwable {
+//        Language.getInstance().loadFromJson(new FileInputStream("../3d-Skin-Layers/Shared/src/main/resources/assets/3dskinlayers/lang/en_us.json"), null);
+        Language lang = TestUtil.loadDefault("/assets/3dskinlayers/lang/en_us.json");
+        SkinLayersModBase.instance = new ObjenesisStd().newInstance(SkinLayersMod.class);
+        SkinLayersModBase.config = new Config();
+        CustomConfigScreen screen = (CustomConfigScreen) SkinLayersModBase.instance.createConfigScreen(null);
+        List<OptionInstance<?>> options = TestUtil.bootStrapCustomConfigScreen(screen);
+        assertNotEquals(screen.getTitle().getString(), lang.getOrDefault(screen.getTitle().getString()));
+        for(OptionInstance<?> option : options) {
+            Set<String> keys = TestUtil.getKeys(option, false);
+            for(String key : keys) {
+                System.out.println(key + " " + lang.getOrDefault(key));
+                assertNotEquals(key, lang.getOrDefault(key));
+            }
         }
-        
-        
-        
     }
 
 }
