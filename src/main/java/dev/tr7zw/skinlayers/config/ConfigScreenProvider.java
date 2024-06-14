6137b282056c1c89630629fe28de254ea64617db
tr7zw <tr7zw@live.de>
2024-06-14 19:49:38 +0000

    Update ConfigScreenProvider.java

--- src/main/java/dev/tr7zw/skinlayers/config/ConfigScreenProvider.java
+++ src/main/java/dev/tr7zw/skinlayers/config/ConfigScreenProvider.java
@@ -28,14 +28,6 @@ public class ConfigScreenProvider {
     public static Screen createConfigScreen(Screen parent) {
         return new CustomConfigScreen(parent, "text.skinlayers.title") {
 
-            // spotless:off
-            //#if MC >= 12100
-            @Override
-            protected void addOptions() {
-            }
-            //#endif
-            //spotless:on
-
             @Override
             public void initialize() {
                 Config config = SkinLayersModBase.config;
