ce65149cdac72845788f6038a0354ecc1727cafb
tr7zw <tr7zw@live.de>
2024-10-01 20:31:30 +0200

    Trying to get it to work with some other mods

--- src/main/java/dev/tr7zw/skinlayers/mixin/ModelPartMixin.java
+++ src/main/java/dev/tr7zw/skinlayers/mixin/ModelPartMixin.java
@@ -3,7 +3,6 @@ package dev.tr7zw.skinlayers.mixin;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.At.Shift;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
@@ -15,19 +14,23 @@ import dev.tr7zw.skinlayers.api.Mesh;
 import dev.tr7zw.skinlayers.api.OffsetProvider;
 import net.minecraft.client.model.geom.ModelPart;
 
-@Mixin(ModelPart.class)
+@Mixin(value = ModelPart.class, priority = 300)
 public class ModelPartMixin implements ModelPartInjector {
 
+    @Shadow
+    boolean visible;
+    
     private Mesh injectedMesh = null;
     private OffsetProvider offsetProvider = null;
 
     // spotless:off
     //#if MC >= 12100
-    @Inject(method = "Lnet/minecraft/client/model/geom/ModelPart;render(Lcom/mojang/blaze3d/vertex/PoseStack;Lcom/mojang/blaze3d/vertex/VertexConsumer;III)V",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/model/geom/ModelPart;translateAndRotate(Lcom/mojang/blaze3d/vertex/PoseStack;)V", shift = Shift.AFTER), cancellable = true)
+    @Inject(method = "Lnet/minecraft/client/model/geom/ModelPart;render(Lcom/mojang/blaze3d/vertex/PoseStack;Lcom/mojang/blaze3d/vertex/VertexConsumer;III)V", at = @At(value = "HEAD"), cancellable = true)
     public void render(PoseStack poseStack, VertexConsumer vertexConsumer, int light, int overlay, int color,
             CallbackInfo ci) {
-        if (injectedMesh != null) {
+        if (visible && injectedMesh != null) {
+            poseStack.pushPose();
+            translateAndRotate(poseStack);
             offsetProvider.applyOffset(poseStack, injectedMesh);
             injectedMesh.render(null, poseStack, vertexConsumer, light, overlay, color);
             poseStack.popPose();
@@ -35,12 +38,15 @@ public class ModelPartMixin implements ModelPartInjector {
         }
     }
     //#else
-    //$$ @Inject(method = "Lnet/minecraft/client/model/geom/ModelPart;render(Lcom/mojang/blaze3d/vertex/PoseStack;Lcom/mojang/blaze3d/vertex/VertexConsumer;IIFFFF)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/model/geom/ModelPart;translateAndRotate(Lcom/mojang/blaze3d/vertex/PoseStack;)V", shift = Shift.AFTER), cancellable = true)
+    //$$ @Inject(method = "Lnet/minecraft/client/model/geom/ModelPart;render(Lcom/mojang/blaze3d/vertex/PoseStack;Lcom/mojang/blaze3d/vertex/VertexConsumer;IIFFFF)V", at = @At(value = "HEAD"), cancellable = true)
     //$$ public void render(PoseStack poseStack, VertexConsumer vertexConsumer, int light, int overlay, float red, float green, float blue, float alpha,
     //$$         CallbackInfo ci) {
-    //$$     if (injectedMesh != null) {
+    //$$     if (visible && injectedMesh != null) {
+    //$$         poseStack.pushPose();
+    //$$         translateAndRotate(poseStack);
     //$$         offsetProvider.applyOffset(poseStack, injectedMesh);
     //$$         injectedMesh.render(null, poseStack, vertexConsumer, light, overlay, red, green, blue, alpha);
+    //$$         poseStack.popPose();
     //$$         ci.cancel();
     //$$     }
     //$$ }
@@ -52,6 +58,11 @@ public class ModelPartMixin implements ModelPartInjector {
     public void setInjectedMesh(Mesh mesh, OffsetProvider offsetProvider) {
         injectedMesh = mesh;
         this.offsetProvider = offsetProvider;
+    }
+    
+    @Shadow
+    public void translateAndRotate(PoseStack poseStack) {
+        
     }
 
 }
